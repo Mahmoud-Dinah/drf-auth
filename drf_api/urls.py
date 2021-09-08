795b28a6a15e89cd0df59460eadd7b78ac90c6a5
mahmoud-dinah <mahmood.dinah2@gmail.com>
2021-09-08 20:44:17 +0300

    added gunicorn

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -24,3 +24,5 @@ urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
